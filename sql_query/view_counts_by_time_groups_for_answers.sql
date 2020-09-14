2fe407689dd6cefb7155672e5746ef11af17d972
ElifKarakutukDinc <elifkaraktk@gmail.com>
2020-09-14 20:24:01 +0200

    adding a query about view time groups

---
+++ sql_query/view_counts_by_time_groups_for_answers.sql
@@ -0,0 +1,10 @@
+--View counts by time groups for answers 
+select
+CASE 
+      WHEN extract('hour' from a_time_new) between 0 and 6 THEN 'night'
+      WHEN extract('hour' from a_time_new) between 7 and 12 THEN 'morning'
+      WHEN extract('hour' from a_time_new) between 13 and 18 THEN 'noon'
+      WHEN extract('hour' from a_time_new) between 19 and 23 THEN 'evening'
+end time_groups_answer , count(distinct answer_id) answer_count
+from sof_2
+group by 1;
