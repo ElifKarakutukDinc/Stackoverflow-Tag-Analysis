a82d7290dccc9fa04c6449c61534fd929bacad45
ElifKarakutukDinc <elifkaraktk@gmail.com>
2020-09-14 20:58:47 +0200

    changed query with aggregations

--- sql_query/view_counts_by_time_groups_for_answers.sql
+++ sql_query/view_counts_by_time_groups_for_answers.sql
@@ -1,10 +1,11 @@
---View counts by time groups for answers 
+--View counts by time groups for question 
 select
 CASE 
-      WHEN extract('hour' from a_time_new) between 0 and 6 THEN 'night'
-      WHEN extract('hour' from a_time_new) between 7 and 12 THEN 'morning'
-      WHEN extract('hour' from a_time_new) between 13 and 18 THEN 'noon'
-      WHEN extract('hour' from a_time_new) between 19 and 23 THEN 'evening'
-end time_groups_answer , count(distinct answer_id) answer_count
+      WHEN extract('hour' from q_time_new) between 0 and 6 THEN 'night'
+      WHEN extract('hour' from q_time_new) between 7 and 12 THEN 'morning'
+      WHEN extract('hour' from q_time_new) between 13 and 18 THEN 'noon'
+      WHEN extract('hour' from q_time_new) between 19 and 23 THEN 'evening'
+end time_groups_answer , Sum( distinct q_view_count) question_view_count
 from sof_2
 group by 1;
+
